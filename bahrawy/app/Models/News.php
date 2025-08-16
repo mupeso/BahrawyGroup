f557a6802101386e53b18327343c5d9f756c42fe
mupeso <ma9620531@gmail.com>
2025-08-16 04:16:28 +0300

    Backup before Laravel 11 upgrade

--- bahrawy/app/Models/News.php
+++ bahrawy/app/Models/News.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class News extends Model
 {
     use HasFactory;
+
+    protected $table = 'new';
 }
--- bahrawy/database/migrations/2025_08_12_145512_create_job_applications_table.php
+++ bahrawy/database/migrations/2025_08_12_145512_create_job_applications_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('subject')->nullable();
             $table->text('comment')->nullable();
-            $table->string('cv_file')->nullable();
+            $table->string('path')->nullable();
 
             $table->timestamps();
         });
